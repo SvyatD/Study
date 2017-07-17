c60d8b7c0f12b617b918489b3612ab1ab8341c29
SvyatD <svyatoslav.dmitriev@mulesoft.com>
2017-07-17 17:48:32 +0300

    Standard start

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -1,9 +1,16 @@
+let { SpecReporter } = require('jasmine-spec-reporter');
+
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['../test/LoginTest.js'],
     onPrepare: () => {
         browser.ignoreSynchronization = true;
+        jasmine.getEnv().addReporter(new SpecReporter({
+          spec: {
+            displayStacktrace: true
+          }
+        }));
     }
 }
 
--- test/LoginTest.js
+++ test/LoginTest.js
@@ -1,23 +1,16 @@
+const { LoginManager, AnypointLandingPage } = require('@mulesoft/automation-coreservices-ui')
+
 describe('Given an authenticated user', () => {
-  const usernameField = $$('input[name="username"]').first();
-  const passwordField = $$('input[name="password"]').first();
-  const signInButton = $$('button[id="form-undefined-primary-action-button"]').first();
-//  const apiManagerHref = $$('a[class="Hero__target___UzNMh"]');
+    beforeAll(() => {
+    browser.get('http://qax.anypoint.mulesoft.com');
+    const loginManager = new LoginManager();
+    const anypointLandingPage = new AnypointLandingPage();
 
+    loginManager.loginAs('jsauto', 'Mule1234');
+    anypointLandingPage.at();
+});
 
-  beforeEach( async () => {
-    await browser.get('http://qax.anypoint.mulesoft.com');
-    await usernameField.sendKeys('jsauto');
-    await passwordField.sendKeys('Mule1234');
-    await signInButton.click();
-  });
-
-  it('main menu page shoud have a title', () => {
-      expect(browser.getTitle()).toEqual('Anypoint Platform');
-  });
-
-//  it('main menu page shoud have a title', async () => {
-//    await apiManagerHref.click()
-//    expect(browser.getTitle()).toEqual('Anypoint Platform for APIs');
-//  });
+    it('shoud have a title', () => {
+        expect(true).toEqual(true);
+    });
 })
